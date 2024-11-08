6f6712d15251d6d8db00245d34fb55df72ef9b13
Md Tauqeer <mdtauqeer525217@gmail.com>
2024-11-08 21:36:37 +0530

    Some changes done

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express'
-import { bookmarkAnyJobs, forgetPassword, getUserById, loginUser, logoutUser, registerNewUser, unbookmarkJob } from '../controller/userController.js';
+import { bookmarkAnyJobs, forgetPassword, getUserById, loginUser, logoutUser, registerNewUser, unbookmarkJob, updatePassword, updateProfile } from '../controller/userController.js';
 import isAuthenticated from '../middleware/isAuthenticated.js';
 const router=new express.Router();
 
 router.post('/user/register',registerNewUser)
       .post('/user/login',loginUser)
       .patch('/user/forget-password',forgetPassword)
+      .patch('/user/update-profile',isAuthenticated,updateProfile)
+      .patch('/user/update-password',isAuthenticated,updatePassword)
       .post('/user/logout',isAuthenticated,logoutUser)
       .post('/user/bookmark/:id',isAuthenticated,bookmarkAnyJobs)  //id= job id
       .patch('/user/unbookmark/:id',isAuthenticated,unbookmarkJob)  //id= job id
